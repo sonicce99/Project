46d21c8a1a43f348cef21ef7bd370b072617fad9
dongsu <sonicce99@naver.com>
2021-09-28 21:43:24 +0900

    My first Toy-project
    `

---
+++ bank-app/js/main.js
@@ -0,0 +1,17 @@
+
+const bankDataRequestURL = 'https://syoon0624.github.io/json/test.json';
+
+const BankDataRequest = new XMLHttpRequest();
+
+// URL로 서버 데이타(json) 연결하기 - response 생성?
+BankDataRequest.open('GET', bankDataRequestURL);
+BankDataRequest.responseType = 'json';
+// 서버에서 response 요청을 보냄
+BankDataRequest.send();
+
+// onload를 통해 응답이 성공적으로 돌아왔을 때 작동할 코드 작성
+BankDataRequest.onload = function () {
+  // BankDataRequest.response는 서버의 json 파일 객체에서 banklist의 배열을 가져옴
+  const BankList = BankDataRequest.response['bankList'];
+
+};
