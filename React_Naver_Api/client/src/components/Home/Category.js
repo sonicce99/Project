3524809ffdc132af80797c0e2ed8be9b2c6fcf48
dongsu <sonicce99@naver.com>
2021-12-20 21:13:15 +0900

    feat: 카테고리별로 쇼핑 목록 렌더링 변경가능하게 수정

--- React_Naver_Api/client/src/components/Home/Category.js
+++ React_Naver_Api/client/src/components/Home/Category.js
@@ -1,42 +1,143 @@
-import { ButtonGroup, DropdownButton, Button, Offcanvas, Dropdown } from 'react-bootstrap'
-import { useState } from 'react';
+import { ButtonGroup, DropdownButton, Button, Fade, Dropdown } from 'react-bootstrap'
+import React, { useState } from 'react';
+import axios from "axios"
 
-const Category = () => {
-  const [show, setShow] = useState(false);
+const Category = ({ setProducts }) => {
+  const [open, setOpen] = useState(false);
+  const [category1, setCategory1] = useState([]);
+  const [category2, setCategory2] = useState([]);
+  const [category3, setCategory3] = useState([]);
+  const [category4, setCategory4] = useState([]);
 
-  const handleClose = () => setShow(false);
-  const handleShow = () => setShow(true);
+  const [category1_Word, setCategory1_Word] = useState("");
+  const [category2_Word, setCategory2_Word] = useState("");
+  const [category3_Word, setCategory3_Word] = useState("");
+
+  const Category_Pick = async (event, word, index) => {
+    event.preventDefault();
+    if (index === 1) {
+      try {
+        setCategory1_Word(word);
+        const data = await axios.post("/category/sort", {
+          query: word
+        })
+        console.log(data.data.items);
+        setProducts(data.data.items);
+      } catch (error) {
+        console.log(error)
+      }
+    }
+    else if (index === 2) {
+      try {
+        setCategory2_Word(word);
+        const data = await axios.post("/category/sort", {
+          query: word
+        })
+        console.log(data.data.items);
+        setProducts(data.data.items);
+      } catch (error) {
+        console.log(error)
+      }
+    }
+    else {
+      try {
+        setCategory3_Word(word);
+        const data = await axios.post("/category/sort", {
+          query: word
+        })
+        console.log(data.data.items);
+        setProducts(data.data.items);
+      } catch (error) {
+        console.log(error)
+      }
+    }
+  }
+
+  const EachCategory = async (event, index) => {
+    event.preventDefault();
+    if (index === 1) {
+      try {
+        const data = await axios.post("/category", {
+          num: 1
+        })
+        setCategory1(data.data.json);
+      } catch (error) {
+        console.log(error)
+      }
+    }
+    else if (index === 2) {
+      try {
+        const data = await axios.post("/category", {
+          num: 2,
+          category1: category1_Word
+        })
+        setCategory2(data.data.json);
+      } catch (error) {
+        console.log(error)
+      }
+    } else if (index === 3) {
+      try {
+        const data = await axios.post("/category", {
+          num: 3,
+          category1: category1_Word,
+          category2: category2_Word
+        })
+        setCategory3(data.data.json);
+      } catch (error) {
+        console.log(error)
+      }
+    } else {
+      try {
+        const data = await axios.post("/category", {
+          num: 4,
+          category1: category1_Word,
+          category2: category2_Word,
+          category3: category3_Word
+        })
+        setCategory4(data.data.json);
+      } catch (error) {
+        console.log(error)
+      }
+    }
+  }
 
   return (
     <>
-      <Button variant="primary" onClick={handleShow}>카테고리별 검색</Button>
-
-      <Offcanvas show={show} onHide={handleClose}>
-        <Offcanvas.Header closeButton>
-          <Offcanvas.Title>Category</Offcanvas.Title>
-        </Offcanvas.Header>
-        <Offcanvas.Body>
-          {['Primary', 'Secondary', 'Success', 'Info', 'Warning', 'Danger'].map(
-            (variant) => (
+      <Button
+        onClick={() => setOpen(!open)}
+        aria-controls="example-fade-text"
+        aria-expanded={open}
+      >
+        카테고리별 검색
+      </Button>
+      <Fade in={open}>
+        <div id="example-fade-text">
+          {['Category1', 'Category2', 'Category3', 'Category4'].map(
+            (variant, i) => (
               <DropdownButton
                 as={ButtonGroup}
                 key={variant}
                 id={`dropdown-variants-${variant}`}
-                variant={variant.toLowerCase()}
-                title={variant}
+                variant={variant.toUpperCase()}
+                title={
+                  variant
+                }
+                onClick={(event) => { EachCategory(event, i + 1) }}
               >
-                <Dropdown.Item eventKey="1">Action</Dropdown.Item>
-                <Dropdown.Item eventKey="2">Another action</Dropdown.Item>
-                <Dropdown.Item eventKey="3" active>
-                  Active Item
-                </Dropdown.Item>
-                <Dropdown.Divider />
-                <Dropdown.Item eventKey="4">Separated link</Dropdown.Item>
+                {
+                  variant === "Category1"
+                    ? category1.map((t, index) => <Dropdown.Item onClick={(event) => { Category_Pick(event, t["category1"], i + 1) }} key={index} eventKey={index + 1}>{t["category1"]}</Dropdown.Item>)
+                    : variant === "Category2"
+                      ? category2.map((t, index) => <Dropdown.Item onClick={(event) => { Category_Pick(event, t["category2"], i + 1) }} key={index} eventKey={index + 1}>{t["category2"]}</Dropdown.Item>)
+                      : variant === "Category3"
+                        ? category3.map((t, index) => <Dropdown.Item onClick={(event) => { Category_Pick(event, t["category3"], i + 1) }} key={index} eventKey={index + 1}>{t["category3"]}</Dropdown.Item>)
+                        : category4.map((t, index) => <Dropdown.Item onClick={(event) => { Category_Pick(event, t["category4"], i + 1) }} key={index} eventKey={index + 1}>{t["category4"]}</Dropdown.Item>)
+                }
               </DropdownButton>
             ),
           )}
-        </Offcanvas.Body>
-      </Offcanvas>
+        </div>
+      </Fade>
     </>
   );
 }
