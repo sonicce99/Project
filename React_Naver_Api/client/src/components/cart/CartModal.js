57a56a1e6bd58799cff0937ab5b6b9526a178117
dongsu <sonicce99@naver.com>
2021-12-22 22:28:06 +0900

    feat: Button 클릭시 장바구니 관련 Modal 창 만듬

---
+++ React_Naver_Api/client/src/components/cart/CartModal.js
@@ -0,0 +1,28 @@
+import React, { useState } from "react";
+import { Modal, Button } from "react-bootstrap";
+import { useHistory } from 'react-router-dom';
+
+const CartModal = ({ isOpen, setIsOpen }) => {
+
+  const handleClose = () => setIsOpen(false);
+  const history = useHistory();
+
+  return (
+    <Modal show={isOpen} onHide={handleClose}>
+      <Modal.Header closeButton>
+        <Modal.Title>장바구니</Modal.Title>
+      </Modal.Header>
+      <Modal.Body>상품이 장바구니에 성공적으로 담겼습니다.</Modal.Body>
+      <Modal.Footer>
+        <Button variant="secondary" onClick={handleClose}>
+          쇼핑을 계속하기
+        </Button>
+        <Button variant="primary" onClick={() => history.push('/cart')}>
+          장바구니로 이동
+        </Button>
+      </Modal.Footer>
+    </Modal>
+  )
+}
+
+export default CartModal;
