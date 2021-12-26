9402ba21fbb6fe0e1862bf684131d3d9b383baba
dongsu <sonicce99@naver.com>
2021-12-26 13:04:44 +0900

    feat: 회원가입, 로그인 로직 추가

--- React_Naver_Api/client/src/components/App.js
+++ React_Naver_Api/client/src/components/App.js
@@ -1,7 +1,7 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import "../App.css";
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { Route } from "react-router-dom";
+import { Route, useLocation } from "react-router-dom";
 
 import Header from "../route/Header";
 import Footer from "../route/Footer";
@@ -10,9 +10,73 @@ import Login from "./login/Login";
 import SignIn from "./signIn/SignIn";
 import Cart from "./cart/Cart";
 import History from "./history/History";
+import cookie from "react-cookies";
+import axios from "axios";
 
 function App() {
-  const [userId, setUserId] = useState("sonicce99@naver.com");
+  const [userId, setUserId] = useState("");
+  const location = useLocation();
+
+  // useEffect(() => {
+  //   if (!("/signIn" === location.pathname || "/" === location.pathname)) {
+  //     fncCheckSession();
+  //   }
+  // }, [location.pathname])
+
+  const fncCheckSession = async () => {
+    const tid = cookie.load("token_id");
+    const tname = cookie.load("token_name");
+    const tpwd = cookie.load("user_password");
+
+    if (tid && tname) {
+      axios.post("/sessionCheck", {
+        token_id: tid,
+        token_name: tname
+      })
+        .then((res) => {
+          console.log(res.data);
+          setUserId(res.data.decrypt_id.user_email);
+
+          if (tpwd) {
+            axios.post("/sessionSignin", {
+              user_email: res.data.decrypt_id.user_email,
+              user_password: tpwd
+            })
+              .then((res) => {
+                console.log(res.data.json[0]);
+                if (!res.data.json[0].user_email) {
+                  // 로그인 해제처리
+                  fncNotLogin();
+                }
+              })
+              .catch((error) => { })
+          } else {
+            // 로그인 해제처리
+            fncNotLogin();
+          }
+        })
+    } else {
+      // 로그인 해제처리
+      fncNotLogin();
+    }
+  }
+
+  // cookie 제거
+  const fncRemoveCookie = () => {
+    cookie.remove("token_id", { path: "/" });
+    cookie.remove("token_name", { path: "/" });
+    cookie.remove("user_password", { path: "/" });
+  }
+
+  // 로그아웃
+  const fncNotLogin = () => {
+    if (window.location.hash !== "nocookie") {
+      fncRemoveCookie();
+    }
+    setTimeout(() => {
+      window.location.href = "/login/#nocookie"
+    }, 1000)
+  }
 
   return (
     <div className="App">
