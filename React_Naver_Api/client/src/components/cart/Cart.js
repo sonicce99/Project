21c1ea407913e05522836d45f38530a4f250cf04
dongsu <sonicce99@naver.com>
2021-12-29 20:12:57 +0900

    feat: CartSum 함수 생성

--- React_Naver_Api/client/src/components/cart/Cart.js
+++ React_Naver_Api/client/src/components/cart/Cart.js
@@ -1,52 +1,87 @@
-import React, { useEffect, useState } from "react";
+import * as React from 'react';
+import { useEffect, useState } from "react";
 import axios from "axios";
+import { DataGrid } from '@mui/x-data-grid';
 
 const Cart = ({ userId }) => {
 
   const [cartList, setCartList] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [sum, setSum] = useState("");
 
   useEffect(() => {
     (async () => {
+      setLoading(true);
       const data = await axios.post("/cart/cartList", {
         user_id: userId
       });
       setCartList(data.data.json);
+      setLoading(false);
     })();
   }, [])
 
+  const CartSum = (event) => {
+    const dataList = event;
+    const sum = cartList.filter((t, i) => dataList.includes(i)).reduce((acc, curr) => acc + Number(curr.l_price), 0);
+    setSum(sum.toString().replace(/\B(?<!\.\d*)(?=(\d{3})+(?!\d))/g, ",") + "원");
+  }
+
+  if (loading) {
+    return (
+      <h2>로딩중</h2>
+    )
+  }
+
+  const columns = [
+    { field: 'id', hide: true, identity: true },
+    {
+      field: 'image',
+      headerName: '제품',
+      headerAlign: 'center',
+      width: 175,
+      editable: true,
+      renderCell: (params) => <img src={params.value} width={150} alt='장바구니이미지' /> // renderCell will render the component
+    },
+    { field: 'title', headerName: '상품정보', headerAlign: 'center', width: 460 },
+    { field: 'l_price', headerName: '가격', headerAlign: 'center', width: 130 },
+    { field: 'ship', headerName: '배송비', headerAlign: 'center', width: 240 }
+  ];
+
+
+  const rows = cartList.map((t, i) => {
+    const a = {
+      id: i,
+      image: t.image,
+      title: t.title,
+      l_price: t.l_price.toString().replace(/\B(?<!\.\d*)(?=(\d{3})+(?!\d))/g, ",") + "원",
+      ship: "동수의 로그북은 언제나 무료 배송!"
+    }
+    return a
+  })
+
+
   return (
-    <table width="1100">
-      <thead>
-        <tr>
-          <th>제품</th>
-          <th>상품명</th>
-          <th>가격</th>
-        </tr>
-      </thead>
-      <tbody>
-        {
-          cartList.map((t, i) => {
-            return (
-              <tr className="cartList" key={i}>
-                <td width="180">
-                  <img className="cartListImg" src={t.image} alt="cartImg" />
-                </td>
-                <td>
-                  <div>
-                    <a href={t.link} target="_blank">
-                      {t.title}
-                    </a>
-                  </div>
-                </td>
-                <td>
-                  <div>{t.l_price.toString().replace(/\B(?<!\.\d*)(?=(\d{3})+(?!\d))/g, ",") + "원"}</div>
-                </td>
-              </tr>
-            )
-          })
-        }
-      </tbody>
-    </table>
+    <div className='cartContainer'>
+      <div style={{ height: 1000, width: '100%' }}>
+        <DataGrid
+          rowHeight={170}
+          rows={rows}
+          columns={columns}
+          pageSize={5}
+          rowsPerPageOptions={[5]}
+          checkboxSelection
+          onSelectionModelChange={CartSum}
+        />
+      </div>
+      <div>
+        <hr className='Sumborder' />
+        <div className='sum'>
+          <span className='sumText'>총 주문금액</span>
+          <span className='sumTotal'>{sum}</span>
+        </div>
+        <hr className='Sumborder' />
+      </div>
+    </div>
   )
 };
 
